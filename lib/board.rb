466aed316013e18d7da383a962aa90efd1b72bcc
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-03-14 16:49:55 -0400

    Add get_set, validate_set, validate_full_house.

--- lib/board.rb
+++ lib/board.rb
@@ -18,8 +18,27 @@ class Board
     @board = Array.new(@labels.length) { Array.new() }
   end
 
+  def get_sets(roll)
+    results = {}
+    roll.each { |die| results[die] = results[die] ? results[die] + 1 : 1 }
+    results
+  end
+
+  def validate_set(roll, category)
+    get_sets(roll).values.include?(category)
+  end
+
+  def validate_full_house(roll)
+    sets = get_sets(roll)
+    sets.values.include?(3) && sets.values.include?(2)
+  end
+
   def score_upper_section(roll, category)
     roll.reduce { |sum, die| sum += die == category ? die : 0 }
+  end
+
+  def score_lower_section(roll, category)
+
   end
 
 
